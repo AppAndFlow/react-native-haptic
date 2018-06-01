c9095cb02fe2045bdf1238170a80cfc74e029678
Koen Punt <koen@koenpunt.nl>
2018-06-01 11:26:08 +0200

    use correct import

--- ios/ReactNativeHaptic.h
+++ ios/ReactNativeHaptic.h
@@ -8,7 +8,11 @@
  */
 
 #import <Foundation/Foundation.h>
-#import <RCTBridgeModule.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 
 @interface ReactNativeHaptic : NSObject<RCTBridgeModule>
 
