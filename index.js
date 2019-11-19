7d1ce9e8d9ee94accd47cc7da8d810e8d983e405
Wesley Scholl <clients@konjoweb.com>
2019-11-19 13:10:57 -0500

    added comments and updated timing patterns

--- index.js
+++ index.js
@@ -2,23 +2,26 @@
 import { NativeModules, Platform, Vibration } from 'react-native';
 
 class ReactNativeHaptic {
+    // default type is 'selection' if no parameter is given
     static generate = (type = 'selection') => {
+        // checking if ios is greater than or equal to 10.0 (to utilize haptic engine)
         if (Platform.OS === "ios" && parseInt(Platform.Version, 10) >= 10) {
             NativeModules.ReactNativeHaptic.generate(type)
         } else {
+            // if platform is android, comparable vibration patterns based on type
             if (Platform.OS === "android") {
                 switch (type) {
                     case "selection":
                         Vibration.vibrate(22)
                         break;
                     case "impactLight":
-                        Vibration.vibrate(25)
-                        break;
-                    case "impactMedium":
                         Vibration.vibrate(30)
                         break;
+                    case "impactMedium":
+                        Vibration.vibrate(40)
+                        break;
                     case "impactHeavy":
-                        Vibration.vibrate(35)
+                        Vibration.vibrate(50)
                         break;
                     case "notification":
                         Vibration.vibrate([10, 40, 60, 30])
@@ -36,7 +39,8 @@ class ReactNativeHaptic {
                         Vibration.vibrate(22)
                         break;
                 }
-            } else if (Platform.OS === "ios") {
+                // vibration fallback for ios lower than 10.0
+            } else if (Platform.OS === "ios" && parseInt(Platform.Version, 10) < 10) {
                 Vibration.vibrate()
             }
         }
